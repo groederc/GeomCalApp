3406421748e1a3726d450c9c9d5ed98a8834d399
Guillermo Martín Roeder Carbo <groederc@users.noreply.github.com>
2026-08-19 13:18:59 -0600

    Update main.py
    final final

--- main.py
+++ main.py
@@ -322,7 +322,7 @@ def main():
         "Introducir las coordenadas de los vértices de la figura en sentido"
         " antihorario.\nPara cerrar la figura, incluir al final el primer"
         " vértice.\nEl programa automáticamente calcula los valores al ir"
-        " introduciendo datos.\n\nAutores:\n    Dr. Guillermo M. Roeder Carbo"
+        " introduciendo datos.\n\nAutor:\n    Dr. Guillermo M. Roeder Carbo"
     )
 
     st.markdown("### Tabla de coordenadas de vértices de la figura")
